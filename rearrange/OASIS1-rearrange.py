67b36f33c841dc65db9684daef2d8350787c106b
sutummala <66123502+sutummala@users.noreply.github.com>
2023-08-27 12:24:51 +0530

    OASIS4-rearrange.py

--- rearrange/OASIS1-rearrange.py
+++ rearrange/OASIS1-rearrange.py
@@ -1,12 +1,12 @@
 # Code created by Sudhakar on Aug 2023
-# code to re-arrange files 
+# code to re-arrange files from OASIS4 dataset
 
 import os
 import shutil
-import nibabel as nb
+import json
 
-data_path = 'C:/Users/drsuc/Downloads/oasis1'
-data_path_new = 'C:/Users/drsuc/Downloads/oasis1Re'
+data_path = 'C:/Users/drsuc/Downloads/oasis4'
+data_path_new = 'C:/Users/drsuc/Downloads/oasis4Re'
 
 if not os.path.exists(data_path_new):
     os.makedirs(data_path_new)
@@ -16,26 +16,43 @@ subjects = os.listdir(data_path)
 for subject in subjects:
     print(f're-arranging for {subject}')
     
-    file_path = os.path.join(data_path, subject, 'RAW')
+    folders_path = os.path.join(data_path, subject)
     file_path_new = os.path.join(data_path_new, subject, 'anat')
     
     if not os.path.exists(file_path_new):
         os.makedirs(file_path_new)
     
-    files = os.listdir(file_path)
-    
-    # Converting .img files to nifti.
-    # for file in files:
-    #     if file.endswith('1_anon.img'):
-    #         file_path_1 = os.path.join(file_path, file)
-    #         img = nb.load(file_path_1)
-    #         nb.save(img, file_path_1.replace('.img', '.nii'))
+    folders = os.listdir(folders_path)
+    T1 = 0; T2 = 0
+    for folder in folders:
+        if folder.startswith('anat'):
+            sub_folder_path = os.path.join(folders_path, folder)
+            for sub_folder in os.listdir(sub_folder_path):
+                if sub_folder.startswith('B'):
+                    for json_file in os.listdir(os.path.join(sub_folder_path, sub_folder)):
+                        json_data = open(os.path.join(sub_folder_path, sub_folder, json_file))
+                        data = json.load(json_data)
+                        series_des = data['SeriesDescription']
+                        if series_des.find('MPRAGE') != -1:
+                            print(f'found MPRAGE at {folder} for {subject}')
+                            T1 = T1 + 1
+                            if T1 <= 1:
+                                image_path = os.path.join(sub_folder_path, 'NIFTI')
+                                for image in os.listdir(image_path):
+                                    print(image)
+                                    shutil.copy(os.path.join(image_path, image), file_path_new)
+                                    file_new = image.replace('T1w', 'hrT1')
+                                    os.rename(os.path.join(file_path_new, image), os.path.join(file_path_new, file_new))
+                        elif series_des.find('TRA_T2') != -1:
+                            print(f'found T2 at {folder} for {subject}')
+                            T2 = T2 + 1
+                            if T2 <= 1:
+                                image_path = os.path.join(sub_folder_path, 'NIFTI')
+                                for image in os.listdir(image_path):
+                                    print(image)
+                                    shutil.copy(os.path.join(image_path, image), file_path_new)
+                                    file_new = image.replace('T2w', 'hrT2')
+                                    os.rename(os.path.join(file_path_new, image), os.path.join(file_path_new, file_new))
         
-    for file in files:
-        if file.endswith('.nii'):
-            print(f'found {file}')
-            shutil.copy(os.path.join(file_path, file), file_path_new)
-            file_new = file.replace(file[14:], 'hrT1.nii')
-            os.rename(os.path.join(file_path_new, file), os.path.join(file_path_new, file_new))
             
 print('re-arrangement done sucessfully')
